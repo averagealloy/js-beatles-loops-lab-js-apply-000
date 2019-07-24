645e01cb3b0d1ae0286e753ab14fc1c0b7df054e
Mike McTighe <m.mctighe10@gmail.com>
2019-07-24 17:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(array{
+  new Array(4);
+for(let i=0; i<theBeatlesPlay;i++){
+    filledArray[i] = { musicians: instruments};
+}
+}
