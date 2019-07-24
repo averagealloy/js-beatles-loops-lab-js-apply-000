8e941a4c5c41be3a416505e4eb6c724dccad1d66
Mike McTighe <m.mctighe10@gmail.com>
2019-07-24 17:48:54 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,7 +0,0 @@
-// add solution here
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-let theBeatlesPlay = new Array(4);
-for(let i=0; i<theBeatlesPlay;i++){
-    filledArray[i] = { musicians: instruments};
-}
