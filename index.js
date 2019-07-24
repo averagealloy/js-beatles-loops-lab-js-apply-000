825bf781d99296494dfcd6c0eea1332457f7e2bd
Mike McTighe <m.mctighe10@gmail.com>
2019-07-24 18:34:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function theBeatlesPlay (musicians , instruments){
 for(var i = 0; i < musicians.length;i++){
-  
+  return musicians[i];
+}
+for(var i = 0; i < instruments.length;i++){
+  return instruments[i];
 }
   }
